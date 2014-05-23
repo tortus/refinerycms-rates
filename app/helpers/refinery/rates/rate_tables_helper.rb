7556bc616ab7aee60747207973f43a0d1df60796
William Makley <wmakley@tortus.com>
2014-05-23 16:49:55 -0400

    try helper inclusion method from refinerycms-pages

---
+++ app/helpers/refinery/rates/rate_tables_helper.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+module Refinery
+  module Rates
+    module RateTableHelper
+
+      def replace_rate_tables(content)
+        content.to_str.gsub(/\{\{rate_table\s+([\w\d_]+)}}/) { |match|
+          url = $1.gsub('_', '-')
+          ::Refinery::Rates.content_for(url)
+        }
+      end
+
+    end
+  end
+end
--- lib/refinery/rates/engine.rb
+++ lib/refinery/rates/engine.rb
@@ -7,6 +7,11 @@ module Refinery
       engine_name :refinery_rates
 
       before_inclusion do
+        ::ApplicationController.send :helper, Refinery::Rates::RateTablesHelper
+        Refinery::AdminController.send :helper, Refinery::Rates::RateTablesHelper
+      end
+
+      initializer "refinery.rates register plugin" do
         Refinery::Plugin.register do |plugin|
           plugin.name = "rates"
           plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.rates_admin_rate_tables_path }
