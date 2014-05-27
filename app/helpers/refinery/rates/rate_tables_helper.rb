624178b076d8e06bd6e30957e6ac7cc365f9d84d
William Makley <wmakley@tortus.com>
2014-05-27 11:10:08 -0400

    fix regexp syntax error

--- app/helpers/refinery/rates/rate_tables_helper.rb
+++ app/helpers/refinery/rates/rate_tables_helper.rb
@@ -4,7 +4,7 @@ module Refinery
     module RateTablesHelper
 
       def replace_rate_tables(content)
-        content.to_str.gsub(/(<p>)?\{\{rate_table\s+([\w\d_]+)}}(</p>)?/ { |match|
+        content.to_str.gsub(/(<p>)?\{\{rate_table\s+([\w\d_]+)}}(<\/p>)?/ { |match|
           url = $2.gsub('_', '-')
           content_tag(:div, ::Refinery::Rates.content_for(url))
         }
