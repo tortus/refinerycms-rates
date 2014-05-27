9a8fb776e11977730957f72356eb6b5798365f5c
William Makley <wmakley@tortus.com>
2014-05-27 11:16:14 -0400

    use safebuffer

--- app/helpers/refinery/rates/rate_tables_helper.rb
+++ app/helpers/refinery/rates/rate_tables_helper.rb
@@ -6,7 +6,7 @@ module Refinery
       def replace_rate_tables(content)
         content.to_str.gsub(/(?:#{Regexp.escape('<p>')}\s*)?\{\{rate_table\s+([\w\d_]+)}}(?:\s*#{Regexp.escape('</p>')})?/) { |match|
           url = $1.gsub('_', '-')
-          content_tag(:div, ::Refinery::Rates.content_for(url), :class => 'rate_table')
+          content_tag(:div, raw(::Refinery::Rates.content_for(url)), :class => 'rate_table')
         }
       end
 
