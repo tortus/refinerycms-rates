6b15e54c42af78b59bc6d3f7411bf75b76509d0f
William Makley <wmakley@tortus.com>
2014-05-27 11:07:13 -0400

    prevent rate table from being inside paragraph if possible

--- app/helpers/refinery/rates/rate_tables_helper.rb
+++ app/helpers/refinery/rates/rate_tables_helper.rb
@@ -4,9 +4,9 @@ module Refinery
     module RateTablesHelper
 
       def replace_rate_tables(content)
-        content.to_str.gsub(/\{\{rate_table\s+([\w\d_]+)}}/) { |match|
-          url = $1.gsub('_', '-')
-          ::Refinery::Rates.content_for(url)
+        content.to_str.gsub(/(<p>)?\{\{rate_table\s+([\w\d_]+)}}(</p>)?/ { |match|
+          url = $2.gsub('_', '-')
+          content_tag(:div, ::Refinery::Rates.content_for(url))
         }
       end
 
