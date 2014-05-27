d0e03e0c20feca6bb5ee19314643fe6cdde72936
William Makley <wmakley@tortus.com>
2014-05-27 12:16:51 -0400

    add id to rate table container div

--- app/helpers/refinery/rates/rate_tables_helper.rb
+++ app/helpers/refinery/rates/rate_tables_helper.rb
@@ -6,7 +6,11 @@ module Refinery
       def replace_rate_tables(content)
         content.to_str.gsub(/(?:<p>\s*)?\{\{rate_table\s+([\w\d_]+)}}(?:\s*<\/p>)?/) { |match|
           url = $1.gsub('_', '-')
-          content_tag(:div, raw(::Refinery::Rates.content_for(url)), :class => 'rate_table')
+          content_tag(:div,
+            raw(::Refinery::Rates.content_for(url)),
+            :class => 'rate_table',
+            :id => "rate_table_#{$1}"
+          )
         }
       end
 
