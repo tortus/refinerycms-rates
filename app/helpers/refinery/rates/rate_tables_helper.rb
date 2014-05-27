845b88d018161c932999c12b083aa04db5068199
William Makley <wmakley@tortus.com>
2014-05-27 11:10:47 -0400

    use non-capturing groups for surrounding <p> tags

--- app/helpers/refinery/rates/rate_tables_helper.rb
+++ app/helpers/refinery/rates/rate_tables_helper.rb
@@ -4,8 +4,8 @@ module Refinery
     module RateTablesHelper
 
       def replace_rate_tables(content)
-        content.to_str.gsub(/(<p>)?\{\{rate_table\s+([\w\d_]+)}}(<\/p>)?/ { |match|
-          url = $2.gsub('_', '-')
+        content.to_str.gsub(/(?:<p>)?\{\{rate_table\s+([\w\d_]+)}}(?:<\/p>)?/ { |match|
+          url = $1.gsub('_', '-')
           content_tag(:div, ::Refinery::Rates.content_for(url))
         }
       end
