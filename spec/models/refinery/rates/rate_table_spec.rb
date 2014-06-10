89ca39eecd60dc0f176cf5701427ad95ebd63f7f
William Makley <wmakley@tortus.com>
2014-06-10 10:43:54 -0400

    add specs for RateTable#inline_replacement_tag

--- spec/models/refinery/rates/rate_table_spec.rb
+++ spec/models/refinery/rates/rate_table_spec.rb
@@ -12,6 +12,17 @@ module Refinery
         its(:errors) { should be_empty }
         its(:title) { should == "Refinery CMS" }
       end
+
+      describe "#inline_replacement_tag" do
+        it "returns {{rate_table underscored_url}} when rate table is persisted" do
+          rate_table = FactoryGirl.create(:rate_table, :title => "My Table", :slug => "my-table")
+          rate_table.inline_replacement_tag.should == "{{rate_table my_table}}"
+        end
+        it "returns nil when rate table is not persisted" do
+          rate_table = FactoryGirl.build(:rate_table, :title => "My Table", :slug => "my-table")
+          rate_table.inline_replacement_tag.should be_nil
+        end
+      end
     end
   end
 end
