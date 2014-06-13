86bf4b51496a0507dbcec6b0935f4db3849fcbcd
William Makley <wmakley@tortus.com>
2014-06-13 16:39:35 -0400

    add some rdoc for EffectiveRate and a simple syntax to fetch the date

--- app/models/refinery/rates/effective_date.rb
+++ app/models/refinery/rates/effective_date.rb
@@ -1,5 +1,14 @@
 module Refinery
   module Rates
+    # Singleton row that that represents the last time that a RateTable
+    # was updated. We can't just take updated_at from a RateTable,
+    # because sometimes we want to bump this date without actually
+    # changing anything.
+    #
+    # Fetch the global effective datetime for all rates:
+    #
+    #     Refinery::Rates::EffectiveDate.effective_at
+    #
     class EffectiveDate < Refinery::Core::BaseModel
       self.table_name = 'refinery_rates_effective_dates'
 
@@ -19,6 +28,10 @@ module Refinery
 
       def self.singleton
         first || create!(:effective_at => Time.now)
+      end
+
+      def self.effective_at
+        singleton.try(:effective_at)
       end
     end
   end
