358e6c553f26b65f1900dc4308f58e8d62d835bd
William Makley <wmakley@tortus.com>
2014-12-22 15:51:34 -0500

    add block feature and anchor to automatic rate tables

--- app/helpers/refinery/rates/rates_helper.rb
+++ app/helpers/refinery/rates/rates_helper.rb
@@ -4,19 +4,33 @@ module Refinery
 
       # Because dates are replaced after rates, it is possible to place the effective date
       # tag within a rate table.
-      def replace_rates_tags(content)
-        replace_rates_effective_dates(replace_rate_tables(content))
+      def replace_rates_tags(content, &block)
+        replace_rates_effective_dates(replace_rate_tables(content, &block))
       end
 
       def replace_rate_tables(content)
         content.to_str.gsub(/(?:<p>\s*)?\{\{rate_table\s+([\w\d_]+)}}(?:\s*<\/p>)?/) { |match|
           underscore_url = $1
           url = underscore_url.gsub('_', '-')
-          content_tag(:div,
-            raw(::Refinery::Rates.content_for(url)),
-            :class => 'rate_table',
-            :id => "rate_table_#{underscore_url}"
-          )
+
+          begin
+            rate_table = Refinery::Rates::RateTable.find_with_cache(url)
+          rescue ActiveRecord::RecordNotFound
+            rate_table = nil
+          end
+
+          replacement = if block_given?
+            yield(rate_table)
+          else
+            content_tag(:div, :class => 'rate_table', :id => "rate_table_#{underscore_url}") do
+              buffer = ActiveSupport::SafeBuffer.new
+              buffer << content_tag(:a, nil, :id => url)
+              buffer << rate_table.try(:content) || %([Rate table "#{url}" not found.])
+              buffer
+            end
+          end
+
+          replacement
         } if content
       end
 
--- spec/helpers/refinery/rates/rates_helper_spec.rb
+++ spec/helpers/refinery/rates/rates_helper_spec.rb
@@ -8,7 +8,7 @@ module Refinery
         let(:rate_table) { FactoryGirl.create(:rate_table, :content => "test content") }
         it "replaces {{rate_table underscored_slug}} with the rate_table's content surrounded by a container div" do
           content = "{{rate_table #{rate_table.to_param.to_s.underscore}}}"
-          helper.replace_rate_tables(content).should == %(<div class="rate_table" id="rate_table_#{rate_table.to_param.to_s.underscore}">#{rate_table.content}</div>)
+          helper.replace_rate_tables(content).should == %(<div class="rate_table" id="rate_table_#{rate_table.to_param.to_s.underscore}"><a id="#{rate_table.to_param}"></a>#{rate_table.content}</div>)
         end
 
         it "removes <p> tags around the tag to replace" do
